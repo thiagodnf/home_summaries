594c4e7b4dcbcb384b8a1958e3923f5cd0959463
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2026-02-01 08:36:11 -0500

    using docker compose instead

--- custom_components/home_summaries/config_flow.py
+++ custom_components/home_summaries/config_flow.py
@@ -12,16 +12,18 @@ _LOGGER = logging.getLogger(__name__)
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input = None):
-   
+
+        _LOGGER.info("Hello!!!")
+
         area_reg = async_get_area_registry(self.hass)
         areas = {area.id: area.name for area in area_reg.async_list_areas()}
-        
+
         # There is no area available on home assistant. Just show an error message
         if not areas:
             return self.async_abort(reason="no_areas_found")
 
         options=[{"value": area_id, "label": name} for area_id, name in areas.items()]
-           
+
         data_schema = {
             vol.Required("areas"): selector.SelectSelector(
                 selector.SelectSelectorConfig(
@@ -30,31 +32,33 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 )
             )
         }
-        
+
         # The user clicked on "Submit"
         if user_input is not None:
-            
+
+            _LOGGER.info("User clicked on Submit with the following input: %s", user_input)
+
             area_ids = user_input["areas"]
-            
+
             # The user did not select any area. Just show an error message
             if not area_ids:
-                
+
                 return self.async_show_form(
-                    step_id = "user", 
+                    step_id = "user",
                     errors = {"areas": "no_areas_selected"},
                     data_schema = vol.Schema(data_schema)
                 )
-            
+
             # The user selected at least 1 area. Let them proceed
             entries = [
                 {"id": area_id, "name": areas.get(area_id, "Unknown Area")} for area_id in area_ids
             ]
-            
+
             return self.async_create_entry(
                 title = INTEGRATION_NAME,
                 data = {
-                    "areas": entries 
+                    "areas": entries
                 }
             )
-            
+
         return self.async_show_form(step_id="user", data_schema=vol.Schema(data_schema))
