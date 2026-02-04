f20ffd2b85e1ca7e536e341751c12a472cc80794
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2026-02-04 10:42:45 -0500

    some testing

--- custom_components/home_summaries/config_flow.py
+++ custom_components/home_summaries/config_flow.py
@@ -4,6 +4,8 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.helpers import selector
 from homeassistant.helpers.area_registry import async_get as async_get_area_registry
+from homeassistant.helpers import label_registry as lr
+
 
 from .const import DOMAIN, INTEGRATION_NAME
 
@@ -11,116 +13,119 @@ _LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
-  async def async_step_user(self, user_input = None):
-    """
-      This method is called every time the user wants to add this integration.
-    """
+  async def async_step_user(self, user_input=None):
+    """Called every time the user wants to add this integration"""
 
-    # # Check if an instance is already configured
-    # if self._async_current_entries():
-    #     return self.async_abort(reason="single_instance_allowed")
+    await self.async_create_summary_label_if_not_exists()
 
-    return await self.async_step_areas()
+    entries = self.hass.config_entries.async_entries("home_summaries")
 
-  async def async_step_areas(self, user_input = None):
+    # used_area_ids = set()
 
-      entries = self.hass.config_entries.async_entries("home_summaries")
+    # for entry in entries:
+    #   used_area_ids.update(area['id'] for area in entry.data["areas"])
 
-      used_area_ids = set()
+    area_reg = async_get_area_registry(self.hass)
+    areas = {area.id: area.name for area in area_reg.async_list_areas()}
 
-      for entry in entries:
-        used_area_ids.update(area['id'] for area in entry.data["areas"])
+    # available_areas = {id: name for id, name in areas.items() if id not in used_area_ids}
 
-      area_reg = async_get_area_registry(self.hass)
-      areas = {area.id: area.name for area in area_reg.async_list_areas()}
-
-      available_areas = {id: name for id, name in areas.items() if id not in used_area_ids}
-
-      # There is no area available on home assistant. Just show an error message
-      if not areas:
-          return self.async_abort(reason="no_areas_found")
-
-      data_schema = {
-        vol.Required("areas"): selector.SelectSelector(
-          selector.SelectSelectorConfig(
-            options = [{"value": area_id, "label": name} for area_id, name in available_areas.items()],
-            multiple = True,
-          )
-        ),
-        # vol.Required("areas"): selector.AreaSelector(
-        #     selector.AreaSelectorConfig(
-        #       multiple=True
-        #     )
-        # )
-      }
-
-      # The user clicked on "Submit"
-      if user_input is not None:
-
-          _LOGGER.info("User clicked on Submit with the following input: %s", user_input)
-
-          area_ids = user_input["areas"]
-
-          # The user did not select any area. Just show an error message
-          if not area_ids:
-
-              return self.async_show_form(
-                  step_id = "user",
-                  errors = {"areas": "no_areas_selected"},
-                  data_schema = vol.Schema(data_schema)
-              )
-
-          # The user selected at least 1 area. Let them proceed
-          entries = [
-              {"id": area_id, "name": areas.get(area_id, "Unknown Area")} for area_id in area_ids
-          ]
-
-          return self.async_create_entry(
-              title = INTEGRATION_NAME,
-              data = {
-                  "areas": entries
-              }
-          )
-          # return await self.async_step_label()
-
-      return self.async_show_form(step_id="areas", data_schema=vol.Schema(data_schema))
-
-  async def async_step_label(self, user_input = None):
-
-    print(user_input)
+    # There is no area available on home assistant. Just show an error message
+    if not areas:
+        return self.async_abort(reason="no_areas_found")
 
     data_schema = {
-      vol.Required("label"): selector.LabelSelector(
-        selector.LabelSelectorConfig(
-          multiple=False
-        )
-      )
+      # vol.Required("areas"): selector.SelectSelector(
+      #   selector.SelectSelectorConfig(
+      #     options = [{"value": area_id, "label": name} for area_id, name in available_areas.items()],
+      #     multiple = True,
+      #   )
+      # ),
+      vol.Required("areas"): selector.AreaSelector({"multiple": True}),
+      vol.Required("label"): selector.LabelSelector({"multiple": False})
     }
 
     # The user clicked on "Submit"
     if user_input is not None:
 
-      area_ids = user_input["areas"]
+        _LOGGER.info("User clicked on Submit with the following input: %s", user_input)
 
-      # The user did not select any area. Just show an error message
-      if not area_ids:
+        area_ids = user_input["areas"]
 
-          return self.async_show_form(
-              step_id = "user",
-              errors = {"areas": "no_areas_selected"},
-              data_schema = vol.Schema(data_schema)
-          )
+        # The user did not select any area. Just show an error message
+        if not area_ids:
 
-      # The user selected at least 1 area. Let them proceed
-      entries = [
-          {"id": area_id, "name": areas.get(area_id, "Unknown Area")} for area_id in area_ids
-      ]
+            return self.async_show_form(
+                step_id = "user",
+                errors = {"areas": "no_areas_selected"},
+                data_schema = vol.Schema(data_schema)
+            )
 
-      return self.async_create_entry(
-          title = INTEGRATION_NAME,
-          data = {
-              "areas": entries
-          }
+        # The user selected at least 1 area. Let them proceed
+        entries = [
+            {"id": area_id, "name": areas.get(area_id, "Unknown Area")} for area_id in area_ids
+        ]
+
+        return self.async_create_entry(
+            title = INTEGRATION_NAME,
+            data = {
+                "areas": entries
+            }
+        )
+        # return await self.async_step_label()
+
+    return self.async_show_form(step_id="user", data_schema=vol.Schema(data_schema))
+
+  # async def async_step_label(self, user_input = None):
+
+  #   print(user_input)
+
+  #   data_schema = {
+  #     vol.Required("label"): selector.LabelSelector(
+  #       selector.LabelSelectorConfig(
+  #         multiple=False
+  #       )
+  #     )
+  #   }
+
+  #   # The user clicked on "Submit"
+  #   if user_input is not None:
+
+  #     area_ids = user_input["areas"]
+
+  #     # The user did not select any area. Just show an error message
+  #     if not area_ids:
+
+  #         return self.async_show_form(
+  #             step_id = "user",
+  #             errors = {"areas": "no_areas_selected"},
+  #             data_schema = vol.Schema(data_schema)
+  #         )
+
+  #     # The user selected at least 1 area. Let them proceed
+  #     entries = [
+  #         {"id": area_id, "name": areas.get(area_id, "Unknown Area")} for area_id in area_ids
+  #     ]
+
+  #     return self.async_create_entry(
+  #         title = INTEGRATION_NAME,
+  #         data = {
+  #             "areas": entries
+  #         }
+  #     )
+
+  #   return self.async_show_form(step_id="label", data_schema=vol.Schema(data_schema))
+
+  async def async_create_summary_label_if_not_exists(self):
+
+    label_reg = lr.async_get(self.hass)
+    label = label_reg.async_get_label_by_name("Summary")
+
+    if label is None:
+
+      label = label_reg.async_create(
+          name="Summary",
+          icon="mdi:text-box-outline",
+          color="#bd93f9",
+          description="Use this label to include the sensor in Home Summaries"
       )
-
-    return self.async_show_form(step_id="label", data_schema=vol.Schema(data_schema))
