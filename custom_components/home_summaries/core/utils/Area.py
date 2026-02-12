1b2940e072c7810cd199cd6c00db355a226ed0d3
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2026-02-12 10:11:03 -0500

    added home summaries for sensors

--- custom_components/home_summaries/core/utils/Area.py
+++ custom_components/home_summaries/core/utils/Area.py
@@ -5,5 +5,15 @@ def get_all_area_ids(hass: HomeAssistant) -> list:
   area_reg = get_area_registry(hass)
   return [area.id for area in area_reg.async_list_areas()]
 
-def get_all_area_ids_except(hass: HomeAssistant, excludes: list[str]):
+def get_all_area_ids_except(hass: HomeAssistant, excludes: list[str]) -> list:
   return [area_id for area_id in get_all_area_ids(hass) if area_id not in excludes]
+
+def get_all_target_area_ids(hass: HomeAssistant, area_id: str) -> list:
+  """
+    If `area_id` is "home", all area _ids except "home" are returned.
+    Otherwise, a list containing only the specified `area_id` is returned.
+  """
+  if area_id == "home":
+    return get_all_area_ids_except(hass, ["home"])
+  else:
+    return [area_id]
--- custom_components/home_summaries/sensor.py
+++ custom_components/home_summaries/sensor.py
@@ -3,13 +3,10 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.components.sensor import SensorDeviceClass
-from homeassistant.helpers.area_registry import async_get as get_area_registry
 
 from .core.utils.Device import get_or_create_device
-from .core.utils.Entity import get_entities_by_area_id_and_label_id
-from .core.utils.Entity import filter_entities_by
 from .core.utils.Entity import get_all_entities_by_label_id, filter_entries_by_area_id, filter_entries_by_device_class
-from .core.utils.Area import get_all_area_ids, get_all_area_ids_except
+from .core.utils.Area import get_all_target_area_ids
 
 from .core.sensors.AverageHumidity import AverageHumidity
 from .core.sensors.AverageTemperature import AverageTemperature
@@ -22,7 +19,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
   area_ids = entry.data.get("area_ids")
   label_id = entry.data.get("label_id")
 
-  all_area_ids = get_all_area_ids(hass)
   entries = get_all_entities_by_label_id(hass, label_id)
 
   sensors = []
@@ -37,40 +33,33 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
   _LOGGER.info("Setup complete for all [sensor] entities")
 
-async def set_up_area_id(hass: HomeAssistant, device, ent) -> list:
+async def set_up_area_id(hass: HomeAssistant, device, entries: list) -> list:
 
   _LOGGER.info("Setting up %s", device.area_id)
 
-  target_area_ids = [device.area_id]
+  target_area_ids = get_all_target_area_ids(hass, device.area_id)
 
-  if device.area_id == "home":
-    target_area_ids = get_all_area_ids_except(hass, ["home"])
+  target_entries = []
 
-  entries = []
+  for target_area_id in target_area_ids:
+    target_entries += filter_entries_by_area_id(hass, entries, target_area_id)
 
-  for area_id in target_area_ids:
-    entries += filter_entries_by_area_id(hass, ent, area_id)
+  onlyHumidSensors = filter_entries_by_device_class(hass, target_entries, SensorDeviceClass.HUMIDITY)
+  onlyTempSensors = filter_entries_by_device_class(hass, target_entries, SensorDeviceClass.TEMPERATURE)
 
-  return await set_up_sensors(hass, device, entries)
-
-async def set_up_sensors(hass: HomeAssistant, device, entries) -> list:
-
-  onlyHumidSensors = filter_entries_by_device_class(hass, entries, SensorDeviceClass.HUMIDITY)
-  onlyTempSensors = filter_entries_by_device_class(hass, entries, SensorDeviceClass.TEMPERATURE)
-
-  humidity_entity_ids = [entry.entity_id for entry in onlyHumidSensors]
-  temperature_entity_ids = [entry.entity_id for entry in onlyTempSensors]
+  humid_entity_ids = [entry.entity_id for entry in onlyHumidSensors]
+  temp_entity_ids = [entry.entity_id for entry in onlyTempSensors]
 
   sensors = []
   humidSensor = None
   tempSensor = None
 
-  if humidity_entity_ids:
-    humidSensor = AverageHumidity(hass, device, humidity_entity_ids)
+  if humid_entity_ids:
+    humidSensor = AverageHumidity(hass, device, humid_entity_ids)
     sensors.append(humidSensor)
 
-  if temperature_entity_ids:
-    tempSensor = AverageTemperature(hass, device, temperature_entity_ids)
+  if temp_entity_ids:
+    tempSensor = AverageTemperature(hass, device, temp_entity_ids)
     sensors.append(tempSensor)
 
   if tempSensor and humidSensor:
